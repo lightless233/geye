a96e207c158c3e6f7a07eed29e7053306a92ba80
lightless <lightless@foxmail.com>
2018-10-15 21:10:31 +0800

    [ADD] Add MultiThreadEngine.

--- geye/core/engine/base/thread.py
+++ geye/core/engine/base/thread.py
@@ -12,9 +12,11 @@
     :license:   GPL-3.0, see LICENSE for more details.
     :copyright: Copyright (c) 2017 lightless. All rights reserved
 """
+import multiprocessing
 import threading
-import abc
 import typing
+
+import abc
 
 from .base import CommonBaseEngine
 
@@ -23,6 +25,10 @@ if typing.TYPE_CHECKING:
 
 
 class SingleThreadEngine(CommonBaseEngine):
+    """
+    基于单线程的引擎模块
+    """
+
     def __init__(self, app_ctx: GeyeApplication, name=None):
         super(SingleThreadEngine, self).__init__()
         self.name = name if name else "SingleThreadEngine"
@@ -39,6 +45,41 @@ class SingleThreadEngine(CommonBaseEngine):
 
     def is_alive(self):
         return self.thread.is_alive()
+
+    @abc.abstractmethod
+    def _worker(self):
+        pass
+
+
+class MultiThreadEngine(CommonBaseEngine):
+    """
+    基于多线程的基础引擎
+    """
+
+    def __init__(self, app_ctx: GeyeApplication, name=None, pool_size=None):
+        super(MultiThreadEngine, self).__init__()
+        self.name = name if name else "MultiThreadEngine"
+        self.app_ctx: GeyeApplication = app_ctx
+        self.pool_size = pool_size if pool_size else multiprocessing.cpu_count() * 2 + 1
+
+    def start(self):
+        self.thread_pool: typing.List[threading.Thread] = \
+            [threading.Thread(
+                target=self._worker, name="{}-{}".format(self.name, idx)
+            ) for idx in range(self.pool_size)]
+        _ = [t.start() for t in self.thread_pool]
+        self.status = self.EngineStatus.RUNNING
+
+    def stop(self, force=True):
+        if not force:
+            while not self.app_ctx.MessageQueues.SEARCH_TASK_QUEUE.empty():
+                self.ev.wait(1)
+                continue
+        self.status = self.EngineStatus.STOP
+        self.ev.set()
+
+    def is_alive(self):
+        return any([t.is_alive() for t in self.thread_pool])
 
     @abc.abstractmethod
     def _worker(self):
--- geye/core/engine/search.py
+++ geye/core/engine/search.py
@@ -2,9 +2,10 @@
 # -*- coding:utf-8 -*-
 
 """
+    geye.core.engine.search
+    ~~~~~~~~~~~~~~~~~~~~~~~
 
-    ~~~~~~~~~~~~~~~~~~
-
+    使用search rule在github上进行第一次初步搜索
 
     :author:    lightless <root@lightless.me>
     :homepage:  None
@@ -12,4 +13,5 @@
     :copyright: Copyright (c) 2017 lightless. All rights reserved
 """
 
-from .thread import SingleThreadEngine, MultiThreadEngine
+
+
