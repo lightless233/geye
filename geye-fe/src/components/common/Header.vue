6dac80b925a76006dc4261b1858a7c85d4dfdece
lightless <lightless@foxmail.com>
2018-12-03 21:13:34 +0800

    [FIX] update author info.

--- geye-fe/src/components/common/Header.vue
+++ geye-fe/src/components/common/Header.vue
@@ -24,10 +24,10 @@
                         {{username}} <i class="el-icon-caret-bottom"></i>
                     </span>
           <el-dropdown-menu slot="dropdown">
-            <a href="http://blog.gdfengshuo.com/about/" target="_blank">
+            <a href="https://lightless.me/about.html" target="_blank">
               <el-dropdown-item>关于作者</el-dropdown-item>
             </a>
-            <a href="https://github.com/lin-xin/vue-manage-system" target="_blank">
+            <a href="https://github.com/redstone-project/geye/" target="_blank">
               <el-dropdown-item>项目仓库</el-dropdown-item>
             </a>
             <el-dropdown-item divided command="loginout">退出登录</el-dropdown-item>
