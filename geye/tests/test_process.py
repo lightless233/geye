06766c101886c0b28891d98b582afef78ce2c907
lightless <lightless@foxmail.com>
2018-12-16 21:20:42 +0800

    [UPDATE] update readme

--- geye/tests/test_process.py
+++ geye/tests/test_process.py
@@ -17,33 +17,53 @@ import time
 import multiprocessing
 
 
-def worker():
-    print("start long job!")
-    time.sleep(15)
-    print("end long job!")
-
-
-p = multiprocessing.Process(target=worker)
-p.start()
-
-# try:
-#     print("start join!")
-#     p.join(5)
-#     print("join done!")
-# except multiprocessing.TimeoutError:
-#     print("timeout!")
+# def worker():
+#     print("start long job!")
+#     time.sleep(15)
+#     print("end long job!")
+#
+#
+# p = multiprocessing.Process(target=worker)
+# p.start()
+#
+# # try:
+# #     print("start join!")
+# #     p.join(5)
+# #     print("join done!")
+# # except multiprocessing.TimeoutError:
+# #     print("timeout!")
+# #     p.terminate()
+#
+# # 下面这段是正确的杀掉子进程的方法
+# # 确认下queue会不会导致内存泄露
+# p.join(5)
+# if p.is_alive():
+#     print("still alive! kill it!")
 #     p.terminate()
+#     p.join()
 
-# 下面这段是正确的杀掉子进程的方法
-# 确认下queue会不会导致内存泄露
-p.join(5)
-if p.is_alive():
-    print("still alive! kill it!")
-    p.terminate()
-    p.join()
+def child_worker():
+    print("child worker start!")
+    time.sleep(15)
+    print("child worker stop!")
 
+
+def parent_worker():
+    print("parent worker start!")
+    p = multiprocessing.Process(target=child_worker)
+    p.start()
+    p.join(5)
+    if p.is_alive():
+        p.terminate()
+        p.join()
+        print("kill confirmed!")
+    print("parent worker stop!")
+
+
+parent_process = multiprocessing.Process(target=parent_worker)
+parent_process.start()
+parent_process.join()
 
 print("main done!")
-
 while True:
     time.sleep(1)
